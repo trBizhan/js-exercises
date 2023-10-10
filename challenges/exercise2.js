763d79eba1338f6462d6541e97ce3f87f060925a
trBizhan <145677592+trBizhan@users.noreply.github.com>
2023-10-10 16:24:05 +0100

    feat: complet exercise 2

--- challenges/exercise2.js
+++ challenges/exercise2.js
@@ -8,29 +8,47 @@ export function getSquares(nums) {
 
 export function camelCaseWords(words) {
   if (words === undefined) throw new Error("words is required");
-  // Your code here!
-  if (words.length == 1) return words.toLowerCase(slice(0, 1));
-  /*
+
+  let nWord = "";
   for (let i = 0; i < words.length; i++) {
-    words[i].toLowerCase(slice(0, 1));
+    if (i == 0) nWord = words[0].charAt(0).toLowerCase() + words[0].slice(1);
+    else nWord += words[i].charAt(0).toUpperCase() + words[i].slice(1);
   }
-  return words;
-  */
+  return nWord;
 }
 
 export function getTotalSubjects(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+
+  let numSubs = 0;
+
+  for (let i = 0; i < people.length; i++) {
+    if (people[i]["subjects"] !== "") {
+      numSubs = numSubs + people[i]["subjects"].length;
+    }
+  }
+  return numSubs;
 }
 
 export function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
-  // Your code here!
+
+  for (let i = 0; i < menu.length; i++) {
+    if (menu[i]["ingredients"].includes(ingredient)) return true;
+  }
+  return false;
 }
 
 export function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
-  // Your code here!
+
+  let mArray = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i])) mArray.push(arr1[i]);
+  }
+
+  mArray.sort((a, b) => a - b);
+  return Array.from(new Set(mArray));
 }
